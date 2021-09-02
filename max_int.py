6a35f61920166bb76dcd83c9646a8ee2c8a81d50
unknown <danielsg21@ru.is>
2021-09-02 19:06:59 +0000

    Algorithm and code for max_int

--- max_int.py
+++ max_int.py
@@ -1,18 +1,12 @@
 #Print out the maximum positive integer input by the user
-
 #Ask the user to input a number
-
 #create a while loop that is True as long as the users' input is a positive number
-
 #Store the maximumum number in the while loop
 #if num_int is larger then max_int
 #max_int becomes num_int
-
 #Once the loop becomes False, print out the maximum number
 
 num_int = int(input("Input a number: "))    # Do not change this line
-# Fill in the missing code
-
 
 max_int = 0
 
